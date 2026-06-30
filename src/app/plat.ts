2398ab7f8f5ddf7ac6fac687240210e04a3ff27b
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-06-30 20:01:48 +0200

    Ajouter les données des pizzas, salades et desserts dans le fichier plat.ts

---
+++ src/app/plat.ts
@@ -0,0 +1,129 @@
+export const pizzas = [
+  {
+    name: "CLASSICA",
+    prices: {
+      junior: 8.00,
+      senior: 10.00,
+      mega: 15.00
+    }
+  },
+  {
+    name: "REGINA",
+    prices: {
+      junior: 9.50,
+      senior: 13.00,
+      mega: 18.00
+    }
+  },
+  {
+    name: "SICILIENNE",
+    prices: {
+      junior: 9.50,
+      senior: 13.00,
+      mega: 18.00
+    }
+  },
+  {
+    name: "CALZONE (soufflée)",
+    prices: {
+      junior: 9.50,
+      senior: 13.00,
+      mega: 18.00
+    }
+  },
+  {
+    name: "ORIENTALE",
+    prices: {
+      junior: 9.50,
+      senior: 13.00,
+      mega: 18.00
+    }
+  },
+  {
+    name: "4 SAISONS",
+    prices: {
+      junior: 9.50,
+      senior: 13.00,
+      mega: 18.00
+    }
+  },
+  {
+    name: "PAYSANNE",
+    prices: {
+      junior: 9.50,
+      senior: 13.00,
+      mega: 18.00
+    }
+  },
+  {
+    name: "RIMINI",
+    prices: {
+      junior: 9.50,
+      senior: 13.50,
+      mega: null
+    }
+  }
+];
+
+
+export const salades = [
+  {
+    name: "PALERMO",
+    description:
+      "Salade de saison, tomates fraîches, poivrons grillés, artichauts, mozzarella",
+    price: 8.0,
+  },
+  {
+    name: "PARIGI",
+    description:
+      "Salade de saison, tomates fraîches, jambon, champignons",
+    price: 8.0,
+  },
+  {
+    name: "ITALIENNE",
+    description:
+      "Salade de saison, tomates fraîches, thon, œuf dur, olives",
+    price: 8.0,
+  },
+  {
+    name: "FERMIÈRE",
+    description:
+      "Salade de saison, tomates fraîches, poulet, maïs",
+    price: 8.0,
+  },
+  {
+    name: "CHAVIGNOL",
+    description:
+      "Salade de saison, tomates fraîches, crottin en toast, lardons",
+    price: 8.0,
+  },
+  {
+    name: "NORVÉGIENNE",
+    description:
+      "Salade de saison, tomates fraîches, crevettes, saumon fumé",
+    price: 8.0,
+  },
+];
+
+export const desserts = [
+  {
+    name: "TARTE AU DAIM",
+    type: "dessert",
+    price: null,
+  },
+  {
+    name: "BROWNIE",
+    type: "dessert",
+    price: null,
+  },
+  {
+    name: "TIRAMISU",
+    type: "dessert",
+    price: null,
+  },
+  {
+    name: "PANINI NUTELLA",
+    type: "dessert",
+    price: null,
+  },
+];
