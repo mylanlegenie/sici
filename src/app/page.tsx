bced2937d3d917e736f6d996dd59da5f4d678256
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-06-30 16:36:22 +0200

    Modifier la structure de la page d'accueil pour inclure NavBar et changer le fond en rouge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import NavBar from "../app/component/NavBar";
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <main className="bg-red-500">
+      <NavBar />
+    </main>
   );
 }
