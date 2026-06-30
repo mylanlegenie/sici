a74d5dc324daeee00518e01e5ec5becd469715ca
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-06-30 17:00:44 +0200

    Ajouter Menu Page

---
+++ src/app/component/Menu.tsx
@@ -0,0 +1,9 @@
+export default function Menu() {
+  return (
+    <section className="flex items-center justify-center mt-20">
+      <div className="w-20 h-10 border-white border-2 rounded-xl flex items-center justify-center cursor-pointer">
+        <span className="text-white font-semibold ">Pizza</span>
+      </div>
+    </section>
+  );
+}
