14fc485eac4070afa78cfb15acd1fabadb207da2
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-06-30 19:09:55 +0200

    Modifier le composant Menu pour afficher dynamiquement les plats et gérer la sélection

--- src/app/component/Menu.tsx
+++ src/app/component/Menu.tsx
@@ -1,9 +1,33 @@
+"use client";
+import { useState } from "react";
 export default function Menu() {
+  const TYPE_PLATS: string[] = ["Pizza", "Pasta", "Salad", "Dessert", "Drink"];
+  const [selectedPlat, setSelectedPlat] = useState<string | null>(null);
+  const handlePlatClick = (plat: string) => {
+    window.history.pushState(
+      {},
+      "",
+      "?section=menu&plat=" + plat.toLowerCase(),
+    );
+  };
   return (
     <section className="flex items-center justify-center mt-20">
-      <div className="w-20 h-10 border-white border-2 rounded-xl flex items-center justify-center cursor-pointer">
-        <span className="text-white font-semibold ">Pizza</span>
-      </div>
+      {TYPE_PLATS.map((plat) => (
+        <div
+          key={plat}
+          className={`w-25 h-15 border-white border-2 rounded-xl flex items-center justify-center cursor-pointer mx-2 ${plat == selectedPlat ? "bg-white" : ""}`}
+          onClick={() => {
+            setSelectedPlat(plat);
+            handlePlatClick(plat);
+          }}
+        >
+          <span
+            className={`text-xl font-semibold ${plat == selectedPlat ? "text-red-500" : "text-white"}`}
+          >
+            {plat}
+          </span>
+        </div>
+      ))}
     </section>
   );
 }
