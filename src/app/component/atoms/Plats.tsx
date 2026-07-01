7b4c3dc00b58b45f1a8dbe746edfecdfd6b6ee87
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-07-01 10:20:16 +0200

    Refactor le composant Plat pour utiliser une interface pour les props et améliorer la gestion des données des plats

--- src/app/component/atoms/Plats.tsx
+++ src/app/component/atoms/Plats.tsx
@@ -1,7 +1,23 @@
-export default function Plat({ plat }: { plat: string }) {
+import { pizzas } from "../../plat";
+import { desserts } from "../../plat";
+import { salades } from "../../plat";
+
+interface PlatsProps {
+  plat: "Pizza" | "Salad" | "Dessert";
+}
+
+export default function Plat({ plat }: PlatsProps) {
+  const data =
+    plat === "Pizza" ? pizzas : plat === "Salad" ? salades : desserts;
+
   return (
-    <div className="w-25 h-15 border-white border-2 rounded-xl flex items-center justify-center cursor-pointer mx-2">
-      <span className="text-white text-xl font-semibold">{plat}</span>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {data.map((item) => (
+        <div
+          key={item.name}
+          className="mt-10 min-h-80 w-full border-2 border-white bg-white rounded-xl flex items-center justify-center cursor-pointer px-4 py-6 text-center"
+        ></div>
+      ))}
     </div>
   );
 }
