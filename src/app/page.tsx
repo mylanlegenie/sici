74a9c5ba4b81040adb7b24014d594664e135ffd9
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-07-01 11:50:23 +0200

    Modifier la fonction Home pour gérer le nom sélectionné et afficher PlatPage ou Menu en conséquence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import NavBar from "./component/atoms/NavBar";
 import Menu from "./component/MenuPage";
+import PlatPage from "./component/PlatPage";
 
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const { section } = await searchParams;
+  const { section, name } = await searchParams;
+  const selectedName = Array.isArray(name) ? name[0] : name;
 
   return (
     <main className="bg-red-500">
       <NavBar />
-      {section === "menu" && <Menu />}
+      {selectedName ? (
+        <PlatPage name={selectedName} />
+      ) : section === "menu" ? (
+        <Menu />
+      ) : null}
     </main>
   );
 }
