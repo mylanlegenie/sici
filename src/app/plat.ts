2711862772dec71aac5731f20b48582c1fa91ed8
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-07-01 18:17:39 +0200

    Ajoute type dans les plats

--- src/app/plat.ts
+++ src/app/plat.ts
@@ -1,106 +1,116 @@
 export const pizzas = [
   {
     name: "CLASSICA",
+    allergenes: ["gluten", "lait"],
     prices: {
-      junior: 8.00,
-      senior: 10.00,
-      mega: 15.00
-    }
+      junior: 8.0,
+      senior: 10.0,
+      mega: 15.0,
+    },
   },
   {
     name: "REGINA",
+    allergenes: ["gluten", "lait"],
     prices: {
-      junior: 9.50,
-      senior: 13.00,
-      mega: 18.00
-    }
+      junior: 9.5,
+      senior: 13.0,
+      mega: 18.0,
+    },
   },
   {
     name: "SICILIENNE",
+    allergenes: ["gluten", "lait"],
     prices: {
-      junior: 9.50,
-      senior: 13.00,
-      mega: 18.00
-    }
+      junior: 9.5,
+      senior: 13.0,
+      mega: 18.0,
+    },
   },
   {
     name: "CALZONE (soufflée)",
+    allergenes: ["gluten", "lait"],
     prices: {
-      junior: 9.50,
-      senior: 13.00,
-      mega: 18.00
-    }
+      junior: 9.5,
+      senior: 13.0,
+      mega: 18.0,
+    },
   },
   {
     name: "ORIENTALE",
+    allergenes: ["gluten", "lait"],
     prices: {
-      junior: 9.50,
-      senior: 13.00,
-      mega: 18.00
-    }
+      junior: 9.5,
+      senior: 13.0,
+      mega: 18.0,
+    },
   },
   {
     name: "4 SAISONS",
+    allergenes: ["gluten", "lait"],
     prices: {
-      junior: 9.50,
-      senior: 13.00,
-      mega: 18.00
-    }
+      junior: 9.5,
+      senior: 13.0,
+      mega: 18.0,
+    },
   },
   {
     name: "PAYSANNE",
+    allergenes: ["gluten", "lait"],
     prices: {
-      junior: 9.50,
-      senior: 13.00,
-      mega: 18.00
-    }
+      junior: 9.5,
+      senior: 13.0,
+      mega: 18.0,
+    },
   },
   {
     name: "RIMINI",
+    allergenes: ["gluten", "lait"],
     prices: {
-      junior: 9.50,
-      senior: 13.50,
-      mega: null
-    }
-  }
+      junior: 9.5,
+      senior: 13.5,
+      mega: null,
+    },
+  },
 ];
-
 
 export const salades = [
   {
     name: "PALERMO",
     description:
       "Salade de saison, tomates fraîches, poivrons grillés, artichauts, mozzarella",
+    allergenes: ["lait"],
     price: 8.0,
   },
   {
     name: "PARIGI",
-    description:
-      "Salade de saison, tomates fraîches, jambon, champignons",
+    description: "Salade de saison, tomates fraîches, jambon, champignons",
+    allergenes: [],
     price: 8.0,
   },
   {
     name: "ITALIENNE",
-    description:
-      "Salade de saison, tomates fraîches, thon, œuf dur, olives",
+    description: "Salade de saison, tomates fraîches, thon, œuf dur, olives",
+    allergenes: ["poisson", "œuf"],
     price: 8.0,
   },
   {
     name: "FERMIÈRE",
-    description:
-      "Salade de saison, tomates fraîches, poulet, maïs",
+    description: "Salade de saison, tomates fraîches, poulet, maïs",
+    allergenes: [],
     price: 8.0,
   },
   {
     name: "CHAVIGNOL",
     description:
       "Salade de saison, tomates fraîches, crottin en toast, lardons",
+    allergenes: ["gluten", "lait"],
     price: 8.0,
   },
   {
     name: "NORVÉGIENNE",
     description:
       "Salade de saison, tomates fraîches, crevettes, saumon fumé",
+    allergenes: ["crustacés", "poisson"],
     price: 8.0,
   },
 ];
@@ -109,21 +119,25 @@ export const desserts = [
   {
     name: "TARTE AU DAIM",
     type: "dessert",
+    allergenes: ["gluten", "lait", "fruits à coque"],
     price: null,
   },
   {
     name: "BROWNIE",
     type: "dessert",
+    allergenes: ["gluten", "lait", "œuf", "fruits à coque"],
     price: null,
   },
   {
     name: "TIRAMISU",
     type: "dessert",
+    allergenes: ["gluten", "lait", "œuf"],
     price: null,
   },
   {
     name: "PANINI NUTELLA",
     type: "dessert",
+    allergenes: ["gluten", "lait", "fruits à coque"],
     price: null,
   },
 ];
