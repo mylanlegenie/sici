295ed114f0d7a3e6a239820d3b57b4f2e8e96fa8
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-07-02 13:39:11 +0200

    Ajout du composant TypeList pour la sélection de plats et mise à jour de MenuPage

--- src/app/component/MenuPage.tsx
+++ src/app/component/MenuPage.tsx
@@ -1,42 +1,7 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import Plats from "./atoms/ListePlats";
+import TypeList from "../component/atoms/TypeList";
 import { motion } from "motion/react";
 import PlatPage from "./PlatDetailsPage";
-
-type PlatType = "Pizza" | "Salad" | "Dessert";
-function MenuComponent() {
-  const TYPE_PLATS: PlatType[] = ["Pizza", "Salad", "Dessert"];
-  const [selectedPlat, setSelectedPlat] = useState<PlatType>("Pizza");
-  const router = useRouter();
-  const handlePlatClick = (plat: PlatType) => {
-    router.push("?section=menu&plat=" + plat.toLowerCase());
-  };
-  return (
-    <section className="w-[80vw] mx-auto mb-20">
-      <div className="flex items-center justify-center mt-20">
-        {TYPE_PLATS.map((plat) => (
-          <div
-            key={plat}
-            className={`w-25 h-15 border-white border-2 rounded-xl flex items-center justify-center cursor-pointer mx-2 ${plat === selectedPlat ? "bg-white" : ""}`}
-            onClick={() => {
-              setSelectedPlat(plat);
-              handlePlatClick(plat);
-            }}
-          >
-            <span
-              className={`text-xl font-semibold ${plat === selectedPlat ? "text-red-500" : "text-white"}`}
-            >
-              {plat}
-            </span>
-          </div>
-        ))}
-      </div>
-      <Plats platType={selectedPlat} />
-    </section>
-  );
-}
 
 export default function MenuPage({
   selectedName,
@@ -55,7 +20,7 @@ export default function MenuPage({
           <PlatPage name={selectedName} />
         </motion.div>
       ) : section === "menu" ? (
-        <MenuComponent />
+        <TypeList />
       ) : null}
     </main>
   );
---
+++ src/app/component/atoms/TypeList.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Plats from "./ListePlats";
+type PlatType = "Pizza" | "Salad" | "Dessert";
+export default function TypeList() {
+  const TYPE_PLATS: PlatType[] = ["Pizza", "Salad", "Dessert"];
+  const [selectedPlat, setSelectedPlat] = useState<PlatType>("Pizza");
+  const router = useRouter();
+  const handlePlatClick = (plat: PlatType) => {
+    router.push("?section=menu&plat=" + plat.toLowerCase());
+  };
+  return (
+    <section className="w-[80vw] mx-auto mb-20">
+      <div className="flex items-center justify-center mt-20">
+        {TYPE_PLATS.map((plat) => (
+          <div
+            key={plat}
+            className={`w-25 h-15 border-white border-2 rounded-xl flex items-center justify-center cursor-pointer mx-2 ${plat === selectedPlat ? "bg-white" : ""}`}
+            onClick={() => {
+              setSelectedPlat(plat);
+              handlePlatClick(plat);
+            }}
+          >
+            <span
+              className={`text-xl font-semibold ${plat === selectedPlat ? "text-red-500" : "text-white"}`}
+            >
+              {plat}
+            </span>
+          </div>
+        ))}
+      </div>
+      <Plats platType={selectedPlat} />
+    </section>
+  );
+}
