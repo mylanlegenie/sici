dab88569572d8ee5834bda89c8442a4e26a741a2
Anais HACHEMANE <mylanhachemane@gmail.com>
2026-08-04 11:23:39 +0200

    Amélioration de l'accueil et des animations

--- src/app/component/LocationSection.tsx
+++ src/app/component/LocationSection.tsx
@@ -20,11 +20,11 @@ export default function LocationSection() {
       >
         Retrouvez-nous
       </h2>
-      <p className="parapgrahe text-white text-sm sm:text-base md:text-lg mb-6 px-2">
-        Venez découvrir
+      <p className="text-white text-sm sm:text-base md:text-lg mb-6 px-2">
+        Venez découvrir{" "}
         <span className="font-semibold px-2 text-red-300">La Sicilienne</span>
-        Situé à deux pas de votre quartier, notre restaurant vous accueille avec
-        des plats italien et tex-mex préparés avec passion.
+        dans le 12ème arrondissement de Paris. Notre restaurant vous accueille
+        avec des plats italiens et tex-mex préparés avec passion.
       </p>
 
       <div>
@@ -37,6 +37,7 @@ export default function LocationSection() {
           }}
         >
           <iframe
+            title="Localisation de La Sicilienne"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d41986.00609450645!2d2.464657439453113!3d48.874886317986814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6734ae1c8dabd%3A0xff7b1228e00744e4!2sLe%20Tiger%20Asian%20FOOD!5e0!3m2!1sfr!2sfr!4v1753626946544!5m2!1sfr!2sfr"
             className="absolute inset-0 z-0 h-full w-full rounded-xl border-4 border-amber-500"
             loading="lazy"
@@ -45,7 +46,7 @@ export default function LocationSection() {
 
           <MotionImage
             src="/pizza-facade-1.png"
-            alt="facade gauche"
+            alt="Pizza margherita italienne de La Sicilienne aux tomates et au basilic"
             width={487}
             height={362}
             className="absolute top-0 left-0 h-full w-1/2 object-cover z-10"
@@ -56,7 +57,7 @@ export default function LocationSection() {
 
           <MotionImage
             src="/pizza-facade-2.png"
-            alt="facade droite"
+            alt="Pizza margherita italienne de La Sicilienne aux tomates et au basilic"
             width={487}
             height={362}
             className="absolute top-0 right-0 h-full w-1/2 object-cover z-10"
