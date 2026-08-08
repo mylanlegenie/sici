dfc0eb652424cabe5b62e9f05ca127e5915a41a0
Anais HACHEMANE <mylanhachemane@gmail.com>
2026-08-08 15:05:26 +0200

    Amélioration de la section Location : suppression des marges inutiles et mise à jour du texte du bouton pour plus de clarté.

--- src/app/component/LocationSection.tsx
+++ src/app/component/LocationSection.tsx
@@ -7,18 +7,18 @@ export default function LocationSection() {
       <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-3 text-white">
         Retrouvez-nous
       </h2>
-      <p className="text-white text-sm sm:text-base md:text-lg mb-6 px-2">
+      <p className="text-white text-sm sm:text-base md:text-lg mb-6">
         Venez découvrir{" "}
-        <span className="font-semibold px-2 text-red-300">La Sicilienne</span>
-        dans le 12ème arrondissement de Paris. Notre restaurant vous accueille
-        avec des plats italiens et tex-mex préparés avec passion.
+        <span className="font-semibold  text-red-300">La Sicilienne</span> dans
+        le 12ème arrondissement de Paris. Notre restaurant vous accueille avec
+        des plats italiens et tex-mex préparés avec passion.
       </p>
 
       <div>
         <ImageSlide />
         <div className="mx-auto mt-6 w-full max-w-243.5">
           <PerspectiveButton
-            text="Google"
+            text="Intinéraire Google Maps"
             href="https://www.google.com/maps/dir/?api=1&destination=6%20rue%20Dagorno%2C%2075012%20Paris"
           />
         </div>
